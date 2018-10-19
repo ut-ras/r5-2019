887917ff8a6d0e3053c65fb1112c12f34af322d0
Matthew Yu <matthewjkyu@gmail.com>
2018-10-18 20:04:12 -0500

    working DB_SCAN with tests

--- DB_scan/DB_SCAN.py
+++ DB_scan/DB_SCAN.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
+#Author: Matthew Yu and Timothy Bertotti
+
 #DBSCAN
 #parameters
 import cv2
 import numpy as np
-#global const episilon
-episilon = 3;   #3pixels
+import sys
 
+#global const episilon
+episilon = 3   #3pixels
+minSize = 30
+height = 0
+width = 0
 
 #M - Mask
 #e - epsilon tuning var
@@ -13,27 +20,34 @@ def DB_SCAN(M, e, minSize):
     id = 1
     for row in range(0, height):
         for col in range(0, width):
-            if M[row][col] == -1:
-                if Fill(M, row, col, e, id) < minSize:
+            if M[row][col] == 255:
+                ctr = Fill(M, row, col, e, id)
+                print(id,"\t", ctr)
+                if ctr < minSize:
                     Fill(M, row, col, e, 0, id)
                 else:
                     id = id + 1
     return M
 
-def Fill(M, row, col, e, id, oldId=-1):
+def Fill(M, row, col, e, id, oldId=255):
     ctr = 1
-    M[row][col] = id
+    M[row][col] = id*25
     for row2 in range(-e, e):
         for col2 in range(-e, e):
             if (row2*row2 + col2*col2) <= (e*e):
                 if(row+row2) >= 0 and (row+row2) < height and (col+col2) >= 0 and (col+col2) < width and M[row+row2][col+col2] == oldId:
-                    ctr = ctr + Fill(row+row2, col+col2, id, M)
+                    ctr = ctr + Fill(M, row+row2, col+col2, e, id)
     return ctr
 
-img = cv2.imread('test.png',0)
+maskName = sys.argv[1]
+img = cv2.imread(maskName,0)
+width, height = np.shape(img)
+print("width:\t",width,"\theight:\t",height)
+
 ret,thresh_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-M = DB_SCAN(thresh_img, e, 5)
-for row in M:
-    for col in M[row]:
-        print(M)
-    print("\n")
+print(thresh_img, "\n")
+M = DB_SCAN(thresh_img, episilon, minSize)
+#cv2.imshow("res", thresh_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+cv2.imwrite(maskName + '_tested.png', M)
