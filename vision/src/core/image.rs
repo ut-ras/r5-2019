8f6d4a3fdef34102aa056d6bfbf48a68c38c03e8
Tianshu Huang <thetianshuhuang@gmail.com>
2018-10-20 11:55:14 -0500

    Added hsv_to_rgb conversion; finished debugging syntax. Ready to start python integration.

--- vision/src/core/image.rs
+++ vision/src/core/image.rs
@@ -29,50 +29,95 @@ pub struct Pixel { h: i32, s: i32, v: i32, mask: i32 }
 ///
 /// ## Members
 ///
-/// - colorspace: RGB or HSV color space
 /// - width: width of the image, in pixels
-/// - height: height of the image, in pixels
+/// - heightght: height of the image, in pixels
 /// - data: flattened image vector. Each row in the image is appended
 ///     horizontally.
 pub struct Image {
-    colorspace: ColorSpace,
-    width: i32,
-    height: i32,
-    data: Vec<i32>
+    width: u32,
+    height: u32,
+    data: Vec<u32>
+}
+
+
+/// # Convert RGB pixel to HSV pixel
+///
+/// ## Parameters
+/// 
+/// - p : input pixel to modify and convert to HSV
+fn rgb_to_hsv(p: &mut Pixel) {
+    let v = cmp::max(cmp::max(p.h, p.s), p.v);
+    let delta = v - cmp::min(cmp::min(p.h, p.s), p.v);
+
+    let h =
+        if      v == p.h { ((255 * (p.s - p.v) / delta) % 1530) / 6 }
+        else if v == p.s { ((255 * (p.v - p.h) / delta) + 510 ) / 6 }
+        else if v == p.v { ((255 * (p.h - p.s) / delta) + 1020) / 6 }
+        else             { 0 };
+    let s = delta / v;
+
+    p.h = h;
+    p.s = s;
+    p.v = v;
+}
+
+
+/// # Convert HSV pixel to RGB pixel
+///
+/// ## Parameters
+/// - p : input pixel to modify and convert to RGB
+fn hsv_to_rgb(p: &mut Pixel) {
+    let c = p.s * p.v / 255;
+    let x = c * (255 - (((6 * p.h) % 510) - 255).abs()) / 255;
+    let m = p.v - c;
+
+    let (r, g, b) = match p.h {
+        0   ... 42  => (c, x, 0),
+        43  ... 85  => (x, c, 0),
+        86  ... 127 => (0, c, x),
+        128 ... 170 => (0, x, c),
+        171 ... 212 => (x, 0, c),
+        213 ... 255 => (c, 0, x),
+        _           => (0, 0, 0),
+    };
+    
+    p.h = r + m;
+    p.s = g + m;
+    p.v = b + m;
 }
 
 
 impl Image {
 
-	/// # Unpack a packed 32-bit integer
-	///
-	/// ## Parameters
-	///
-	/// - idx : index of the value to fetch
-	///
-	/// ## Returns
-	///
-	/// Unpacked pixel struct
-	fn unpack(&self, idx: i32) -> Pixel {
-		let packed = self.data.get(idx);
+    /// # Unpack a packed 32-bit integer
+    ///
+    /// ## Parameters
+    ///
+    /// - idx : index of the value to fetch
+    ///
+    /// ## Returns
+    ///
+    /// Unpacked pixel struct
+    fn unpack(&self, idx: usize) -> Pixel {
+        let packed = self.data[idx];
         Pixel {
-            h: packed & 0xF000 >> 24,
-            s: packed & 0x0F00 >> 16,
-            v: packed & 0x00F0 >> 8,
-            mask: packed & 0x000F,
+            h: (packed & 0xF000 >> 24) as i32,
+            s: (packed & 0x0F00 >> 16) as i32,
+            v: (packed & 0x00F0 >> 8 ) as i32,
+            mask: (packed & 0x000F)    as i32,
         }
-	}
+    }
 
-	/// # Pack a pixel struct and store
-	///
-	/// ## Parameters
-	///
-	/// - input : pixel value to pack and store
-	/// - idx : destination to store to
-	fn pack(&self, input: Pixel, idx: i32) {
-		self.data[idx] = (
-			input.h << 24 | input.s << 16 | input.v << 8 | input.mask);
-	}
+    /// # Pack a pixel struct and store
+    ///
+    /// ## Parameters
+    ///
+    /// - input : pixel value to pack and store
+    /// - idx : destination to store to
+    fn pack(&mut self, input: &Pixel, idx: usize) {
+        self.data[idx] =
+            (input.h << 24 | input.s << 16 | input.v << 8 | input.mask) as u32;
+    }
 
     /// # Access and unpack an image pixel
     /// Since images are stored as flattened vectors, the target index
@@ -86,11 +131,26 @@ impl Image {
     /// ## Returns
     ///
     /// Unpacked pixel struct
-    pub fn access(&self, x: i32, y: i32) -> Pixel {
+    pub fn access(&self, x: u32, y: u32) -> Pixel {
         debug_assert!(x < self.width);
         debug_assert!(y < self.height);
 
-        unpack(&self, self.width * y + x);
+        self.unpack((self.width * y + x) as usize)
+    }
+
+    /// # Iterate over pixels
+    ///
+    /// ## Parameters
+    ///
+    /// - f : function to call on all pixels; pixels are read and passed into
+    ///     f. f is then allowed to modify the pixel. After, the pixel is
+    ///     packed and stored.
+    pub fn iter_pixels(&mut self, f: &Fn(&mut Pixel)) {
+        for x in 0..self.width * self.height {
+            let mut p = self.unpack(x as usize);
+            f(&mut p);
+            self.pack(&mut p, x as usize);
+        }
     }
 
     /// # Convert color spaces.
@@ -98,31 +158,10 @@ impl Image {
     /// ## Parameters
     ///
     /// - format : target color space; ColorSpace::RGB or ColorSpace::HSV
-    pub fn cvt_color(&self, format: ColorSpace) {
-
-    	if format == ColorSpace::HSV && self.colorspace == ColorSpace::RGB {
-
-    		for x in 0..self.width * self.height {
-    			let pixel = access(&self, x);
-
-    			let v = cmp::max(cmp::max(pixel.h, pixel.s), pixel.v);
-    			let delta = v - cmp::min(cmp::min(pixel.h, pixel.s), pixel.v);
-    			let h = 0;
-    			let h = if v == self.h {
-    				255 * (((pixel.s - pixel.v) / delta) % 6) / 6;
-    			} else if v == self.s {
-    				255 * (((pixel.v - pixel.h) / delta) + 2) / 6;
-    			} else if v == self.v {
-    				255 * (((pixel.h - pixel.s) / delta) + 4) / 6;
-    			};
-    			let s = delta / v;
-
-    			let pixel:h = h;
-    			let pixel:s = s;
-    			let pixel:v = v;
-
-    			pack(&self, pixel, x);
-    		}
-    	}
+    pub fn cvt_color(&mut self, format: ColorSpace) {
+        match format {
+            ColorSpace::HSV => { self.iter_pixels(&rgb_to_hsv); },
+            ColorSpace::RGB => { self.iter_pixels(&hsv_to_rgb); },
+        }
     }
 }
