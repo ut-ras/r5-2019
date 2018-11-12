65bc4f2032c218a49697c4796d2766e9489e0a5d
Matthew Yu <matthewjkyu@gmail.com>
2018-11-11 20:19:53 -0600

    debugged, currently working for at least Test0, Test3, added test 0, needs more testing, finding density/rad constants for ideal, consistent results

--- DB_scan/DB_SCAN2.py
+++ DB_scan/DB_SCAN2.py
@@ -3,21 +3,63 @@
 #           Kiran Raja
 #           Tony Li
 #           Timothy Bertotti
-#Last modified: 11/4/18
+#Last modified: 11/11/18
+#Working parameters for various tests:
+#   Test0 - Rad = 1, Density = 0
+#   Test3 - Rad = 4, Density = 2
 #DBSCAN
 
 #imports
 import cv2
 import numpy as np
 import sys
+import copy
 
 #global consts
-MASK_NAME = null
-MASK = null
-BALL_RAD = 1
-DENSITY = 2
+SOURCE_ROOT = ".Test_Images/"
+MASK_NAME = "test0.png"
+BALL_RAD = 4
+DENSITY = 2     #k*n^2, where k is a experimental fractional constant of the ball radius
 HEIGHT = 0
 WIDTH = 0
+
+def preprocess(mask):
+    """
+    Takes the mask, and changes all white value to -1 (working!) for DB_SCAN
+
+    Parameters
+    ----------
+    mask : int[][]
+        Binary mask - mask to edit
+    """
+    for row in range(0, HEIGHT):
+        for col in range(0, WIDTH):
+            if(mask[row][col] == 255):
+                mask[row][col] = -1
+    return mask
+
+def color(mask):
+    """
+    Takes the mask, finds the number of unique objects, and colors in each object
+    based on that color by changing the ID value (also pixel value).
+    For debugging purposes.
+
+    Parameters
+    ----------
+    mask : int[][]
+        Binary mask - mask to edit
+    """
+    found_id = []
+    for row in range(0, HEIGHT):
+        for col in range(0, WIDTH):
+            if mask[row][col] not in found_id:
+                found_id.append(mask[row][col])
+    num_id = len(found_id)
+    color_delta = 255/num_id
+    for row in range(0, HEIGHT):
+        for col in range(0, WIDTH):
+                mask[row][col] = (mask[row][col] * color_delta)
+    return mask
 
 def merge_ref(mask, found_id, position):
     """
@@ -35,10 +77,10 @@ def merge_ref(mask, found_id, position):
     Note: side effect of merge_ref leaves pixels of the mask skipping IDs if merged
     """
     min_id = min(found_id)
-    for row in range(0, height):
-        for col in range(0, width):
+    for row in range(0, HEIGHT):
+        for col in range(0, WIDTH):
             if mask[row][col] in found_id:
-                mask[row][col] = whole_id
+                mask[row][col] = min_id
             #break after current pixel is reached
             if row == position[0] and col == position[1]:
                 return
@@ -59,7 +101,7 @@ def in_bounds(row,col):
     bool
         true if in bounds, false elsewise
     """
-    return row >= 0 and col >= 0 and row < height and col < width
+    return row >= 0 and col >= 0 and row < HEIGHT and col < WIDTH
 
 def DB_SCAN(mask, radius, density):
     """
@@ -80,8 +122,8 @@ def DB_SCAN(mask, radius, density):
     """
     id = 1
     #mask[row][col] - pixel to transform
-    for row in range(0, height):
-        for col in range(0, width):
+    for row in range(0, HEIGHT):
+        for col in range(0, WIDTH):
             count = 0       #number of object pixels in radius
             found_id = []   #list of unique ids in radius
             #for a neighbor pixel in the ball radius
@@ -89,25 +131,31 @@ def DB_SCAN(mask, radius, density):
                 for c in range(col - radius, col + radius):
                     #mask[r][c] - neighbor pixel (including center pixel)
                     #if pixel pos < ball radius & within the picture & not black
-                    if in_bounds(r,c) and mask[r][c] != O and r*r + c*c <= radius*radius:
+                    xDist = r - row
+                    yDist = c - col
+                    if in_bounds(r,c) and (mask[r][c] != 0) and (yDist*yDist + xDist*xDist <= radius*radius):
                         count = count + 1
                         #if an identified value
-                        if mask[r][c] != -1:
+                        if mask[r][c] != 255:
                             if mask[r][c] not in found_id:
-                                found_id.append([r, c])
+                                found_id.append(mask[r][c])
             #if total number of found related pixels > density
-            if(count > density):
+            # print(mask[row][col], " count: ", count)
+            # if count > 0:
+            #     print("found_ids: ", found_id)
+            if(count > density and mask[row][col] != 0):
                 num_ids = len(found_id)
                 #part of new object
                 if num_ids == 0:
                     mask[row][col] = id
-                    id = id++
+                    id = id + 1
                 #part of same object
                 elif num_ids == 1:
                     mask[row][col] = found_id[0]
                 #if there are more than one objects in the vicinity, merge
                 else:
                     merge_ref(mask, found_id, [row, col])
+                # print("new id: ", mask[row, col])
     return mask
 
 #main
@@ -116,6 +164,10 @@ MASK = cv2.imread(MASK_NAME,0)
 WIDTH, HEIGHT = np.shape(MASK)
 print("width:\t",WIDTH,"\theight:\t",HEIGHT)
 ret,MASK = cv2.threshold(MASK,127,255,cv2.THRESH_BINARY)
-print(MASK, "\n")
+#MASK = preprocess(MASK)
 MASK = DB_SCAN(MASK, BALL_RAD, DENSITY)
-cv2.imwrite(MASK_NAME + '_tested.png', MASK)
+ret = color(MASK)
+cv2.imshow(MASK_NAME.split(".", 1)[0], ret)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite(MASK_NAME.split(".", 1)[0] + '_tested.png', ret)
