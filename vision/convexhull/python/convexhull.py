f88c3a66fa5cdc7ed376436be2d851c64e855b6c
Benjamin Belov <benbelov@gmail.com>
2018-11-25 18:53:45 -0600

    Basic implementation, testing, and documention complete

--- vision/convexhull/python/convexhull.py
+++ vision/convexhull/python/convexhull.py
@@ -1,50 +1,190 @@
 import cv2 as cv 
 import numpy as np
 
-def slope(a, b):
+def slope(a, b, flip):
+    """returns slope between two pixels
+    
+    Parameters
+    ----------
+    a : tuple of ints
+        (x, y) coordinate of first pixel
+    b : tuple of ints
+        (x, y) coordinate of second pixel
+    flip : bool
+        true if slope is needed from a flipped x and y axes
+    
+    Returns
+    -------
+    float
+        slope between pixels
+    """
+
+    
     ax, ay = a
     bx, by = b  
 
-    return (by-ay)/(bx-ax)
+    dx = bx - ax
+    dy = by - ay
+
+    if flip:
+        return -dx / dy
+    else:
+        return dy / dx
 
 
 def convex_hull(image):
-    height, width = image.shape 
+    """finds the convex hull vertecies for a binary image
     
-    convex_points = [(0, width)]
+    Parameters
+    ----------
+    image : binary opencv image
+        image to perform convexhull on
+    
+    Returns
+    -------
+    list of tuples of ints
+        clockwise vertecies of convex hull
+    """
+
+    corners = find_corners(image)
+
+    print("Corners:", corners)
+
+    vertices = [corners[0]]
+
+
+    for i in range(len(corners)):
+        print("Convex hulling points: ", corners[i], corners[(i + 1) % len(corners)])
+        vertices.extend(_convex_hull_side(image, corners[i], corners[(i + 1) % len(corners)]))
+
+    return vertices
+    
+
+def _convex_hull_side(image, start, end):
+    """performs convex hull algorihim on rectanglar subset of an image from one direction
+    
+    Parameters
+    ----------
+    image : binary opencv image
+        image to be processed
+    start : tuple of ints
+        top left corner of subset in current orientation
+    end : tuple of ints
+        bottom right corner of subset in current orientation
+    
+    Returns
+    -------
+    list of tuples of ints
+        convex hull set of vertecies not including the starting point
+    """
+
+    convex_points = [start]
+
+    x1, y1 = start
+    x2, y2 = end
+
+    side = (x1 <= x2, y1 <= y2)
+
+    print(side)
+
+    ranges = {
+        (True, True): [[x1 + 1, x2 + 1], [y1, y2 + 1], False],
+        (False, True): [[y1 + 1, y2 + 1], [x1, x2 - 1, -1], True],
+        (False, False): [[x1 - 1, x2 - 1, -1], [y1, y2 - 1, -1], False],
+        (True, False): [[y1 - 1, y2 - 1, -1], [x1, x2 + 1], True]
+    }
 
     prev = 0
-    for x, row in enumerate(image):
+
+    for outer in range(*ranges[side][0]):
 
         curr_pixel = None
 
-        for y, pixel in enumerate(row):
-            
-            if pixel != 0:
-                curr_pixel = (x, y)
+        for inner in range(*ranges[side][1]):
+            '''if ranges[side][2]:
+                print(inner, outer)
+            else:
+                print(outer, inner)'''
+            if ranges[side][2] and image[outer, inner] == 0:
+                curr_pixel = (inner, outer)
+                break
+            elif not ranges[side][2] and image[inner, outer] == 0:
+                curr_pixel = (outer, inner)
                 break
         
         if curr_pixel is None:
             continue
-
-        print(curr_pixel)
+        
+        print(f"Tst: {curr_pixel}")
 
         while True:
             # slope infinite for first point
-            prev_slope = (float("inf") if prev == 0
-                          else slope(convex_points[prev-1], convex_points[prev]))
+            prev_slope = (float("-inf") if prev == 0
+                          else slope(convex_points[prev-1], convex_points[prev], ranges[side][2]))
+            print(f"Slp: {prev_slope} : {slope(convex_points[prev], curr_pixel, ranges[side][2])}")
             # remove previous point if it yeilds concave hull 
-            if prev_slope < slope(convex_points[prev], curr_pixel):
-                convex_points.pop(prev)
+            if prev_slope > slope(convex_points[prev], curr_pixel, ranges[side][2]):
+                print(f"Del: {convex_points.pop(prev)}")
                 prev -= 1
-            # add point to hull if it yeilds cnvex hull
+            # add point to hull if it yeilds convex hull
             else:
+                print(f"Add: {curr_pixel}")
                 convex_points.append(curr_pixel)
                 prev += 1
                 break
 
-
-    return convex_points 
+    return convex_points[1:]
 
 def find_corners(image):
+    """finds the four exteme pixels of a binary image
+    
+    Parameters
+    ----------
+    image : binary opencv image
+        image of which to find corners of
+    
+    Returns
+    -------
+    list of tuples of ints
+        most top left, top right, bottom right, and bottom left pixels of the image
+    """
 
+    corners = []
+
+    for side in range(4):
+        corners.append(_find_corner(image, side))
+    return corners
+
+def _find_corner(image, corner):
+    """finds the extreme pixel of a binary image in a given direction
+    
+    Parameters
+    ----------
+    image : binary opencv image
+        image to find corner in
+    corner : int
+        0 for top right, 1 for top left, 2 for bottom right, 3 for bottom left
+    
+    Returns
+    -------
+    tuple of ints
+        corner pixel in corner direction
+    """
+
+    # TODO make corner an enum
+
+    height, width = image.shape
+
+    ranges = {
+        0: [[height], [width], True],
+        1: [[width - 1, 0, -1], [height], False],
+        2: [[height - 1, 0, -1], [width - 1, 0, -1], True],
+        3: [[width], [height - 1, 0, -1], False]
+    }
+
+    for outer in range(*ranges[corner][0]):
+        for inner in range(*ranges[corner][1]):
+            if ranges[corner][2] and image[outer, inner] == 0:
+                return (inner, outer)
+            elif not ranges[corner][2] and image[inner, outer] == 0:
+                return (outer, inner)
--- vision/convexhull/python/convexhull_test.py
+++ vision/convexhull/python/convexhull_test.py
@@ -2,8 +2,18 @@ import convexhull
 import cv2
 import numpy as np
 
-img = cv2.imread('../../../../convexHullTests/convexhull_subprocess_test.png', 0)
+img = cv2.imread('../convexHullTests/corner_test_2.png', 0)
 
 result = convexhull.convex_hull(img)
 
+img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+for x, y in result:
+    img[y][x] = [0, 0, 255]
+
 print(result)
+cv2.namedWindow("corners", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("corners", 120, 120)
+cv2.imshow("corners", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
