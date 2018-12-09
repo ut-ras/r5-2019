21ba1deb77296f1be7c9803bab8c8a16b7d63069
Tony Li <tonyli0409@utexas.edu>
2018-12-08 22:55:44 -0600

    added short servo test main, note it might need to be adjusted based on specifications of the servo

--- drivers/core/testmain.cpp
+++ drivers/core/testmain.cpp
@@ -1,12 +1,30 @@
+#include <pigpio.h>
+#include <iostream>
+#include "../inc/motor.h"
+#include "../inc/pid.h"
 
-
-int main(void){motors[3] = new DRV(15, 14, 17, 18);
+int main(void){
+    motors[3] = new DRV(15, 14, 17, 18);
     motors[2] = new DRV(25, 8, 7, 1);
     motors[1] = new DRV(27, 22, 10, 9);
     motors[0] = new DRV(6, 13, 5, 0);
     cout << "Motors initialized" << endl;
     for(int i = 0; i < 4; ++i) {
-        linearControl[i] = new PID(.01, .01, .0003);
+        linearControl[i] = new PID(.01, .01, .0003);            //configs borrowed from pacbot
     }
+    for(int i = 0; i < 4; ++i) {
+        for(int i = -1; i <= 1; i += 0.25){
+            motors[i]->set(i);
+            time_sleep(1);
+            motors[i]->stop();
+        }
     }
-    
+}
+int main2(void){
+    gpioSetMode(18, PI_OUTPUT);
+    for(int i = 0; i <= 2500; i += 500){
+        gpioServo(18, i);
+        cout << gpioGetServoPulsewidth(18);
+        time_sleep(1);
+    }
+}
