404b8de61bf3c60f1f642a5c7d51037772728ce8
dimembermatt <matthewjkyu@gmail.com>
2019-03-13 18:09:45 -0500

    parameter debugging fixed for first half of the cost function

--- vision/gradient_descent2.py
+++ vision/gradient_descent2.py
@@ -9,18 +9,20 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import multivariate_normal
 import random
+
 SAMPLE_SIZE = 2
 
-def cost_function(data, base_point):
+def cost_function(data, other_args):
     """
     Cost function [].
 
     Parameters
     ----------
     data: int[][3]
-        set of points in 3d color space (image)
-    base_point: int[3[]
+        set of points in 3d color space (image) compressed into shape (n,)
+    other_args: [int[3], int]
         reference point in 3d color space (the guess)
+        size of complete sample of image data
 
     Returns
     -------
@@ -28,14 +30,19 @@ def cost_function(data, base_point):
         function evaluation, a new point in 3d color space
     """
     # get a sample (maybe 2% of image pixels) list of pixels
-    sample_list = random.sample(set(data), SAMPLE_SIZE)
+
+    data = data.reshape((other_args[1], 3)) # convert back into 2d array
+    sample_list = random.choices(data, k=SAMPLE_SIZE) # somehow deal with replacement
     # calculate all distances from each pixel to the base point (the guess)
     distances = [[
-        sample[0] - base_point[0],
-        sample[1] - base_point[1],
-        sample[2] - base_point[2]] for sample in sample_list
+        sample[0] - other_args[0][0],
+        sample[1] - other_args[0][1],
+        sample[2] - other_args[0][2]] for sample in sample_list
     ]
+    
     # get the normal distribution of the point distance
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     res = [
         multivariate_normal.pdf([dist[0] for dist in distances]),
         multivariate_normal.pdf([dist[1] for dist in distances]),
@@ -44,19 +51,21 @@ def cost_function(data, base_point):
     # sum the normal distribution in a singular point (difference)
     diff = [0, 0, 0]
     for ele in res:
-        diff[0] += ele[0]    # multiplied by some probability value? (pdf = probability density function)
+        diff[0] += ele[0]
         diff[1] += ele[1]
         diff[2] += ele[2]
 
-    base_point[0] += diff[0]
-    base_point[1] += diff[1]
-    base_point[2] += diff[2]
+    other_args[0][0] += diff[0]
+    other_args[0][1] += diff[1]
+    other_args[0][2] += diff[2]
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-    return base_point
+    return other_args[0]
+
 
 start_guess = [0, 0, 0]
+# replace this variable with a set of pixels of an image
 data_set = np.array([[0, 0, 0], [255, 255, 255], [122, 122, 122]])
-data_set = np.ndarray(shape=(3, 3), buffer=data_set)
-
-res = minimize(cost_function, data_set, start_guess, method='nelder-mead', options={'xtol': 1e-8, 'disp': True})
-print(res)
+# SAMPLE_SIZE = int(data_set.size/3 * .03) # i.e. an image has say 1k pixels, take 3 % of the pixels as the sampling
+res = minimize(cost_function, data_set, [start_guess, int(data_set.size/3)], method='nelder-mead', options={'xtol': 1e-8, 'disp': True})
+print(res.x)
