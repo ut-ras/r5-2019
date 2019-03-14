3061b061d6aaa374f4c19766d94841cd612fbef2
Tianshu Huang <thetianshuhuang@gmail.com>
2019-03-14 13:30:30 -0500

    Switched to class-based

--- vision/fov.py
+++ vision/fov.py
@@ -7,128 +7,170 @@ import math
 import samples
 
 import collections
-Object = collections.namedtuple("Object", ["rect", "dist", "otype"])
-
-WIDTH = 1440
-HEIGHT = int(WIDTH * 0.75)
-ESIZE = int(WIDTH / 50)
-DSIZE = int(ESIZE * 0.8)
-CSIZE = int(ESIZE)
-
-ERODE_MASK = np.ones((ESIZE, ESIZE), np.uint8)
-DILATE_MASK = np.ones((ESIZE, ESIZE), np.uint8)
-CUBE_ERODE_MASK = np.ones((CSIZE * 2, CSIZE * 2), np.uint8)
-CUBE_DILATE_MASK = np.ones((CSIZE, CSIZE), np.uint8)
-
-FOV_H = math.radians(63.54)
-FOV_V = math.radians(42.36)
-CAM_HEIGHT = 5
-
-FIELD_LOWER = np.array([8, 130, 180])
-FIELD_UPPER = np.array([12, 200, 255])
-
-CUBE_LOWER = np.array([0, 0, 170])
-CUBE_UPPER = np.array([255, 120, 255])
+Object = collections.namedtuple("Object", ["rect", "dist", "meta"])
 
 
-def get_field(src):
-    mask = cv2.inRange(src, FIELD_LOWER, FIELD_UPPER)
-    mask = cv2.dilate(cv2.erode(mask, ERODE_MASK), DILATE_MASK)
+class VisionModule():
 
-    hull_fill = np.zeros(mask.shape, dtype=np.uint8)
-    contours, hier = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for c in contours:
-        hull_fill = cv2.fillConvexPoly(hull_fill, cv2.convexHull(c), 255)
+    FOV_H = math.radians(63.54)
+    FOV_V = math.radians(42.36)
+    CAM_HEIGHT = 5
 
-    # bitwise AND with !FIELD
-    mask = cv2.bitwise_and(cv2.bitwise_not(mask), hull_fill)
+    FIELD_LOWER = np.array([8, 130, 180])
+    FIELD_UPPER = np.array([12, 200, 255])
 
-    return mask, hull_fill
+    CUBE_LOWER = np.array([0, 0, 170])
+    CUBE_UPPER = np.array([255, 120, 255])
+
+    def __init__(
+            self, width=640, height=480,
+            erode_ksize=0.02, dilate_ksize=0.016):
+
+        self.erode_ksize = int(erode_ksize * width)
+        self.dilate_ksize = int(dilate_ksize * width)
+        self.width = width
+        self.height = height
+
+        def make_square_kernel(i):
+            return np.ones((i, i), np.uint8)
+
+        self.__erode_mask = make_square_kernel(self.erode_ksize)
+        self.__dilate_mask = make_square_kernel(self.dilate_ksize)
+        self.__cube_erode_mask = make_square_kernel(2 * self.erode_ksize)
+
+    def __get_field_mask(self, src):
+
+        # Threshold
+        mask = cv2.inRange(src, self.FIELD_LOWER, self.FIELD_UPPER)
+
+        # Clean up
+        mask = cv2.erode(mask, self.__erode_mask)
+        mask = cv2.dilate(mask, self.__dilate_mask)
+
+        # Compute and fill convex hull
+        hull_fill = np.zeros(mask.shape, dtype=np.uint8)
+        contours, hier = cv2.findContours(
+            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        for c in contours:
+            hull_fill = cv2.fillConvexPoly(hull_fill, cv2.convexHull(c), 255)
+
+        # bitwise AND with !FIELD
+        mask = cv2.bitwise_and(cv2.bitwise_not(mask), hull_fill)
+
+        return mask
+
+    def __get_object_properties(self, obj, meta):
+
+        x, y, w, h = cv2.boundingRect(obj)
+        try:
+            d = (
+                self.CAM_HEIGHT /
+                math.tan(
+                    self.FOV_V * ((y + h) - self.height / 2) / self.height))
+        except ZeroDivisionError:
+            d = -1
+
+        return Object(rect=[x, y, w, h], dist=d, meta=meta)
+
+    def __mask_to_objects(self, mask, meta):
+
+        contours, hier = cv2.findContours(
+            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        return [self.__get_object_properties(c, meta) for c in contours]
+
+    def __get_objects(self, src, mask):
+
+        cube_mask = cv2.inRange(src, self.CUBE_LOWER, self.CUBE_UPPER)
+
+        cube_mask = cv2.bitwise_and(mask, cube_mask)
+        cube_mask = cv2.dilate(cube_mask, self.__dilate_mask)
+        cube_mask = cv2.erode(cube_mask, self.__cube_erode_mask)
+        cube_mask = cv2.dilate(cube_mask, self.__dilate_mask)
+
+        cubes = self.__mask_to_objects(cube_mask, "cube")
+
+        mask = cv2.bitwise_and(mask, cv2.bitwise_not(cube_mask))
+        mask = cv2.erode(mask, self.__erode_mask)
+        mask = cv2.dilate(mask, self.__dilate_mask)
+
+        obstacles = self.__mask_to_objects(mask, "obstacle")
+
+        return cubes + obstacles
+
+    def process(self, img):
+
+        if img.shape != (self.width, self.height):
+            img = cv2.resize(img, (self.width, self.height))
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+        mask = self.__get_field_mask(img)
+        cubes = self.__get_objects(img, mask)
+
+        return cubes
 
 
-def get_object_properties(c, otype=None):
+WIDTH = 640
+HEIGHT = 480
 
-    x, y, w, h = cv2.boundingRect(c)
-    try:
-        d = CAM_HEIGHT / math.tan(FOV_V * ((y + h) - HEIGHT / 2) / HEIGHT)
-    except ZeroDivisionError:
-        d = 0
-
-    return Object(rect=[x, y, w, h], dist=d, otype=otype)
+COLORS = {
+    "obstacle": (255, 0, 0),
+    "cube": (0, 255, 0)
+}
 
 
-def get_cubes(src, mask):
+def load(tgt):
 
-    cube_mask = cv2.inRange(src, CUBE_LOWER, CUBE_UPPER)
-    cube_mask = cv2.bitwise_and(mask, cube_mask)
-
-    cube_mask = cv2.dilate(cube_mask, CUBE_DILATE_MASK)
-    cube_mask = cv2.erode(cube_mask, CUBE_ERODE_MASK)
-    cube_mask = cv2.dilate(cube_mask, CUBE_DILATE_MASK)
-
-    mask = cv2.bitwise_and(mask, cv2.bitwise_not(cube_mask))
-    contours, hier = cv2.findContours(
-        cube_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    # cv2.drawContours(src, contours, -1, (0, 255, 0), 3)
-
-    cubes = [get_object_properties(c, otype=(0, 255, 0)) for c in contours]
-
-    return mask, cube_mask, cubes
+    return cv2.resize(samples.load(int(tgt)), (WIDTH, HEIGHT))
 
 
-def get_obstacles(src, mask):
-
-    mask = cv2.erode(mask, ERODE_MASK)
-    mask = cv2.dilate(mask, DILATE_MASK)
-
-    contours, hier = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # cv2.drawContours(src, contours, -1, (255, 0, 0), 3)
-
-    return [get_object_properties(c, otype=(255, 0, 0)) for c in contours]
-
-
-def main():
-
-    src = cv2.resize(samples.load(int(sys.argv[1])), (WIDTH, HEIGHT))
-    src_hsv = cv2.cvtColor(src, cv2.COLOR_RGB2HSV)
-
-    print(ESIZE)
-    print(DSIZE)
-
-    import time
-    st = time.time()
-
-    # Get field
-    mask, hull_fill = get_field(src_hsv)
-    # Get cubes
-    mask, cube_mask, cubes = get_cubes(src_hsv, mask)
-    # Get obstacles
-    obstacles = get_obstacles(src, mask)
-
-    print(time.time() - st)
-
-    # Show output
-    for c in cubes + obstacles:
+def draw(img, objects):
+    for c in objects:
         cv2.rectangle(
-            src, (c.rect[0], c.rect[1]),
-            (c.rect[0] + c.rect[2], c.rect[1] + c.rect[3]), c.otype, 3)
-        cv2.putText(
-            src, "{:.2f}".format(c.dist), (c.rect[0], c.rect[1]),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-    plt.imshow(src)
-    plt.show()
+            img,
+            (c.rect[0], c.rect[1]),
+            (c.rect[0] + c.rect[2], c.rect[1] + c.rect[3]),
+            COLORS.get(c.meta), 3)
+        plt.text(
+            c.rect[0], c.rect[1],
+            "{:.2f}".format(c.dist), color=(1, 1, 1))
 
 
 if __name__ == '__main__':
 
+    import time
     import sys
 
-    # from tester import profile
-    # res, s = profile(main, stats=20)
-    # print(s)
+    mod = VisionModule(width=WIDTH, height=HEIGHT)
 
-    main()
+    if sys.argv[1] == 'all':
+        total = 0
+        for i in range(9):
+
+            img = load(i)
+
+            start = time.time()
+            objects = mod.process(img)
+            dur = time.time() - start
+
+            print(dur)
+            total += dur
+
+            plt.subplot(330 + i + 1)
+            draw(img, objects)
+            plt.imshow(img)
+
+        print("Avg time: {}ms".format(total / 9 * 1000))
+        plt.show()
+
+    else:
+        img = load(int(sys.argv[1]))
+
+        start = time.time()
+        objects = mod.process(img)
+        print(time.time() - start)
+
+        draw(img, objects)
+        plt.imshow(img)
+        plt.show()
