96259ae69d73a2e586303abcf2311d285a3a3776
stefandebruyn <stefandebruyn@utexas.edu>
2019-04-01 19:15:45 -0500

    Add camera support to RobotState

--- tools/simulator/r5engine/robotcontrol.py
+++ tools/simulator/r5engine/robotcontrol.py
@@ -197,18 +197,20 @@ class RobotState:
     their best to mimic them.
     """
     def __init__(self, drive_state=None, drive_magnitude=0, claw_state=False,
-        elevator_state=False):
+        elevator_state=False, camera_state=False):
         """
         Parameters
         ----------
         drive_state: int
             drive state type; see VALID_STATES
+        drive_magnitude: float
+            unsigned magnitude of the drive
         claw_state: bool
             whether or not the claw is engaged
         elevator_state: bool
             whether or not the elevator is raised
-        magnitude: float
-            magnitude of the state, usually a velocity
+        camera_state: bool
+            whether or not the camera is up
         """
         if drive_state not in VALID_DRIVE_STATES:
             raise ValueError("invalid state", drive_state)
@@ -217,15 +219,16 @@ class RobotState:
         self.drive_magnitude = drive_magnitude
         self.claw_state = claw_state
         self.elevator_state = elevator_state
+        self.camera_state = camera_state
 
     def __str__(self):
         """
         Returns
         -------
         str
-            string representation of the form (IDENTITY@MAGNITUDE)
+            string representation
         """
-        format = "({0}@{1:0.3f}, claw={2}, elev={3})"
+        format = "({0}@{1:0.3f}, claw={2}, elev={3}, cam={4})"
         return format.format(DRIVE_STATE_ID_LOOKUP[self.drive_state],
             self.drive_magnitude, str(self.claw_state),
-            str(self.elevator_state))
+            str(self.elevator_state), str(self.camera_state))
