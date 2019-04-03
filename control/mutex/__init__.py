cedb2d1a8d2ac54d3090024ef70bf8b7366d1928
Tianshu Huang <thetianshuhuang@gmail.com>
2019-04-02 21:13:38 -0500

    Added main files

--- control/mutex/__init__.py
+++ control/mutex/__init__.py
@@ -1,5 +1,6 @@
 """Mutex Controller Module"""
 
 from .handler import MutexController
+from .controller import make_server
 
-__all__ = [	"MutexController"]
+__all__ = [	"MutexController", "make_server"]
---
+++ settings.py
@@ -0,0 +1,11 @@
+"""Settings file
+
+This is imported by both main_base.py and main_agent.py. Place all settings
+here in order to avoid having to write values twice.
+"""
+
+# Server for semaphore task server
+MUTEX_SERVER_PORT = 8000
+
+# IP of task manager
+TASK_MANAGER_IP = '192.168.5.1'
