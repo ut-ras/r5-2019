15db8ceea6465b28d7c1ae9fa56db21a4f0c9e86
Tianshu Huang <thetianshuhuang@gmail.com>
2019-04-03 21:09:51 -0500

    Added VisionModuleThread to __init__.

--- vision/__init__.py
+++ vision/__init__.py
@@ -1,6 +1,7 @@
 from .camera import Camera, capture_test
 from .main import VisionModule
+from .vision_thread import VisionModuleThread
 
 __all__ = [
-    "Camera", "VisionModule", "capture_test"
+    "Camera", "VisionModule", "VisionModuleThread", "capture_test"
 ]
