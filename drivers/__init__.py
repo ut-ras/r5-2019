b0e2382afd06409d7702858988e9d35cdf9a8825
Tianshu Huang <thetianshuhuang@gmail.com>
2019-04-05 00:32:13 -0500

    Missing '.'

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -1,6 +1,6 @@
 from .swarmbot import SwarmBot
 from .io import LED1, LED2, LED3, LED4, RST, ESTOP, GP2, GP3
-from core import RobotFrame, RobotState, TURN, DRIVE, init, drive
+from .core import RobotFrame, RobotState, TURN, DRIVE, init, drive
 
 __all__ = [
     "SwarmBot",
