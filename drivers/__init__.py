ed02943aa1a1502151904e3f92ea36b0b5d90361
Tianshu Huang <thetianshuhuang@gmail.com>
2019-04-05 00:32:57 -0500

    Typo: drive->move

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -1,10 +1,10 @@
 from .swarmbot import SwarmBot
 from .io import LED1, LED2, LED3, LED4, RST, ESTOP, GP2, GP3
-from .core import RobotFrame, RobotState, TURN, DRIVE, init, drive
+from .core import RobotFrame, RobotState, TURN, DRIVE, init, move
 
 __all__ = [
     "SwarmBot",
     "LED1", "LED2", "LED3", "LED4",
     "RST", "ESTOP", "GP2", "GP3",
-    "RobotFrame", "RobotState", "TURN", "DRIVE", "init", "drive"
+    "RobotFrame", "RobotState", "TURN", "DRIVE", "init", "move"
 ]
